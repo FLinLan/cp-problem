de54b2892f4ca38b050b294a1eb70fd56fdc582b
FLinLan <maxlan0987@gmail.com>
2024-06-16 23:07:34 -0700

    added blind 75 folder and a better template

--- template.cpp
+++ template.cpp
@@ -1,103 +1,267 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <utility>
-#include <cmath>
-#include <map>
-#include <unordered_map>
-#include <unordered_set>
 #include <set>
-#include <queue>
-#include <stack>
-#include <limits>
-#include <numeric>
-#include <cstring>  // For memset
-#include <climits> // For INT_MAX, INT_MIN
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <cmath>
+#include <string>
+#include <utility>
 
 using namespace std;
 
-// For fast input and output
-#define fast_io ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-
-// Shortcuts for commonly used types
 typedef long long ll;
-typedef unsigned long long ull;
-typedef vector<int> vi;
+typedef int INT;
+#define int ll
 typedef pair<int, int> pii;
-typedef vector<pii> vii;
+typedef pair<ll, ll> pll;
 
-// Constants
-const int INF = 1e9 + 7;
-const int MOD = 1e9 + 7;
-
-// Macros for convenience
-#define pb push_back
-#define mp make_pair
-#define all(x) x.begin(), x.end()
-#define fi first
-#define se second
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define rrep(i, a, b) for (int i = a; i >= b; i--)
-
-// Utility functions
-template <typename T>
-void print_vector(const vector<T>& v) {
-    for (const auto& elem : v) {
-        cout << elem << ' ';
-    }
-    cout << '\n';
+namespace predefined_vars {
+    string sp = " ";
+    string nl = "\n";
+    int e1 = 10;
+    int e2 = 100;
+    int e3 = 1000;
+    int e4 = 10000;
+    int e5 = 100000;
+    int e6 = 1000000;
+    int e7 = 10000000;
+    int e8 = 100000000;
+    int e9 = 1000000000;
+    int e10 = 10000000000;
+    int e11 = 100000000000;
+    int e12 = 1000000000000;
+    int e13 = 10000000000000;
+    int e14 = 100000000000000;
+    int e15 = 1000000000000000;
+    int e16 = 10000000000000000;
+    int e17 = 100000000000000000;
+    int e18 = 1000000000000000000;
 }
 
-template <typename T>
-void read_vector(vector<T>& v) {
-    for (auto& elem : v) {
-        cin >> elem;
+using namespace predefined_vars;
+
+namespace linked_list {
+    struct linkl {
+        int x;
+        linkl *prev = NULL, *next = NULL;
+    };
+
+    linkl* newlink(int x, linkl *a = NULL, linkl *b = NULL) {
+        linkl *c = new linkl;
+        c->x = x;
+        c->prev = a;
+        c->next = b;
+        return c;
+    }
+
+    void insert(linkl *a, int x) {
+        linkl *b = newlink(x, a, a->next);
+        if (a->next != NULL) a->next->prev = b;
+        a->next = b;
+    }
+
+    void del(linkl *a) {
+        if (a->prev != NULL && a->next != NULL) {
+            a->prev->next = a->next;
+            a->next->prev = a->prev;
+        } else if (a->prev != NULL) {
+            a->prev->next = NULL;
+        } else {
+            a->next->prev = NULL;
+        }
+        delete a;
+    }
+
+    void print(linkl *a) {
+        while (a != NULL) {
+            cout << a->x << " ";
+            a = a->next;
+        }
+        cout << "\n";
+    }
+
+    bool contains(linkl *a, int x) {
+        bool found = false;
+        while (a != NULL) {
+            if (a->x == x) {
+                found = true;
+                break;
+            }
+            a = a->next;
+        }
+        return found;
     }
 }
 
-// Utility function to read a grid 
-template <typename T>
-void read_grid(vector<vector<T>>& grid, int h, int w) {
-    for (int i = 0; i < h; ++i) {
-        for (int j = 0; j < w; ++j) {
-            cin >> grid[i][j];
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int random(int mod) {
+    return rng() % mod;
+}
+
+int hehehash() {
+    int x = 69 * random(420);
+    int y = 420 * random(69);
+    int z = x * y;
+    for (int i = 0; i < 69; i++) {
+        z += random(e13);
+    }
+    int a = 694206942069420 % random(694206942069420);
+    return random(e18) + (z + a) + 69420;
+}
+
+bool is_sorted(vector<int>& vec) {
+    int n = vec.size();
+    for (int i = 1; i < n; i++) {
+        if (vec[i] < vec[i - 1]) return false;
+    }
+    return true;
+}
+
+int gcd(int x, int y) {
+    while (y != 0) {
+        int t = y;
+        y = x % y;
+        x = t;
+    }
+    return x;
+}
+
+int lcm(int x, int y) {
+    return (x * y) / gcd(x, y);
+}
+
+void sort2(vector<int>& x, vector<int>& y, bool g = false) {
+    int n = x.size();
+    vector<pii> z(n);
+    for (int i = 0; i < n; i++) {
+        z[i] = {x[i], y[i]};
+    }
+    if (!g) sort(z.begin(), z.end());
+    else sort(z.begin(), z.end(), greater<pii>());
+    for (int i = 0; i < n; i++) {
+        x[i] = z[i].first;
+        y[i] = z[i].second;
+    }
+}
+
+double shoelace(vector<pii> coords) {
+    int n = coords.size();
+    double area = 0.0;
+    int j = n - 1;
+    for (int i = 0; i < n; i++) {
+        area += (coords[j].first + coords[i].first) * (coords[j].second - coords[i].second);
+        j = i;
+    }
+    return abs(area / 2.0);
+}
+
+namespace printout {
+
+    void po(vector<int>& vec) {
+        for (auto x : vec) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+
+    void po(vector<string>& vec, string sep = "\n") {
+        for (auto x : vec) {
+            cout << x << sep;
+        }
+        if (sep != "\n") cout << "\n";
+    }
+
+    void po(vector<pii>& vec) {
+        for (auto x : vec) {
+            cout << x.first << " " << x.second << "\n";
         }
     }
-}
 
-// Utility function to print a grid
-template <typename T>
-void print_grid(const vector<vector<T>>& grid, int h, int w) {
-    for (int i = 0; i < h; ++i) {
-        for (int j = 0; j < w; ++j) {
-            cout << grid[i][j] << ' ';
+    void po(multiset<int>& ms) {
+        for (auto x : ms) {
+            cout << x << " ";
         }
-        cout << '\n';
+        cout << "\n";
     }
 }
 
-// Main function to solve individual test cases
+void srt(vector<int>& vec) {
+    sort(vec.begin(), vec.end());
+}
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+using namespace printout;
+
+int power2(int x) {
+    int y = 1;
+    for (int i = 0; i < x; i++) {
+        y *= 2;
+    }
+    return y;
+}
+
+int sum(vector<int>& vec) {
+    int sm = 0;
+    for (int i = 0; i < vec.size(); i++) {
+        sm += vec[i];
+    }
+    return sm;
+}
+
+void in(vector<int>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        cin >> vec[i];
+    }
+}
+
+void in(vector<char>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        cin >> vec[i];
+    }
+}
+
+void in(vector<string>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        cin >> vec[i];
+    }
+}
+
+void in(vector<pii>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        cin >> vec[i].first >> vec[i].second;
+    }
+}
+
+bool letter(char c) {
+    if (c >= 'a' && c <= 'z') return true;
+    return false;
+}
+
+bool num(char c) {
+    if (c >= '0' && c <= '9') return true;
+    return false;
+}
+
+void lower(string &s) {
+    for (int i = 0; i < s.size(); i++) {
+        s[i] = tolower(s[i]);
+    }
+}
+
 void solve() {
-    // Example: Read number of elements in a vector and the vector itself
-    int n;
-    cin >> n;
-    vi v(n);
-    read_vector(v);
     
-    // Example processing: Sort the vector and print it
-    sort(all(v));
-    print_vector(v);
 }
 
-int main() {
-    fast_io;
-
-    int t;
-    cin >> t; // Read number of test cases
-
-    while (t--) {
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    while (T--) {
         solve();
     }
-
-    return 0;
 }
